132b66e438e22401250fd2e13af8a3f788f17027
vinipolitta <vinicius.politta1@gmail.com>
2022-06-13 11:50:59 -0300

    Adicionar arquivos de projeto.

---
+++ vns-trips-backend/Controllers/MarketController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vns_trips_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MarketController : ControllerBase
+    {
+        public MarketController()
+        {
+          
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "value";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "valeu post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return $"valeu put: {id}";
+
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return $"valeu del: {id}";
+        }
+    }
+}
